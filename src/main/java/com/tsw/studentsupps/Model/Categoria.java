87a1087151597a37007cfa5567d6b4d91a32e36a
FireLion137 <55352707+FireLion137@users.noreply.github.com>
2023-05-26 11:44:00 +0200

    Inizio Progetto

---
+++ src/main/java/com/tsw/studentsupps/Model/Categoria.java
@@ -0,0 +1,28 @@
+package com.tsw.studentsupps.Model;
+
+public class Categoria {
+    private String id;
+    private String nome;
+    private String descrizione;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id= id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome= nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione= descrizione;
+    }
+}
